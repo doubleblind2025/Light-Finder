268097af4168427167f0188e06c01844aaf64fdf
doubleblind2025 <sharp1129@163.com>
2024-08-16 23:18:47 +0800

    init

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
 
     BenchMark<string ,int32_t> dataset("./data/sample.dat", 1600, 1000);
     dataset.SketchError(10);
-    //dataset.TopKError(0.00005);
+    dataset.TopKError(0.00005);
     return 0;
 }
